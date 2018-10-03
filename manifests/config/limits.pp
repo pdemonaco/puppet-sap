52de53389d0ebdbbca0c3066eed33e49c4fddcc3
Phil DeMonaco <pdemon@gmail.com>
2018-10-03 16:03:27 -0400

    Hiera based SID Pattern
    Limits substitution wasn't using the pattern specified in hiera. This
    corrects that problem!

--- manifests/config/limits.pp
+++ manifests/config/limits.pp
@@ -14,10 +14,14 @@ class sap::config::limits {
       $sequence = $parameters['sequence']
       $header_comment = $parameters['header_comment']
       $entries = $parameters['entries']
+      $per_sid = $parameters['per_sid']
 
       # If this is an SID specific file we need to transform our input structure
       # using the SID pattern
-      if $parameters['per_sid'] {
+      if $per_sid {
+        # Retrieve target pattern
+        $sid_pattern = $parameters['sid_pattern']
+
         # Replace the SID pattern in the user/group name with the system SID
         # TODO - maybe only do this for keys which match the pattern?
         $entries_transformed = $entries.map | $entry | {
@@ -26,7 +30,7 @@ class sap::config::limits {
 
           # Create a new pair per entry
           $per_sid_array = $sap::system_ids.map | $sid | {
-            $sid_group = regsubst($base_group, '_sid_', downcase($sid), 'G')
+            $sid_group = regsubst($base_group, $sid_pattern, downcase($sid), 'G')
 
             # Return value for the mapping
             $new_entry = [$sid_group, $data]
